499396c252d893e6941c43f79a05b185e5d0af73
SimoneFerrini <ferrini.simone4@gmail.com>
2023-02-24 15:47:15 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+
+Scrivi un programma che stampi in console i
+ numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” 
+ al posto del numero e per i multipli di 5 stampi “Buzz”.
+ Per i numeri che sono sia multipli di 3 che di 5 stampi 
+ “FizzBuzz”.
+
+-selezionare il contenitore
+-creare 100 elementi numerati
+-via via che si creano controllare se divisibili x 5,3 o entrambi
+- se si inserire il corrispettivo colore e testo
+- se no inserire il numero dell'elemento
+
+ */
+
